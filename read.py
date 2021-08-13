96996fbee223bf4f6e3255fc9369e67e0aed561c
昊辰 <haochen1204@aliyun.com>
2021-08-13 17:27:59 +0800

    add

--- read.py
+++ read.py
@@ -1,9 +1,11 @@
 import sys
 import os
+import webbrowser
 
 path = str(sys.argv[1])
 find = str(sys.argv[2])
-print(path+'+++'+find)
+num=0
+#print(path+'+++'+find)
 #path = input("请输入日志所在目录路径：")
 #path = 'D:\work\恒睿达\www\日志'
 #find = input("请输入查找的内容：")
@@ -13,7 +15,7 @@ for i in get_dir:
     try:
         i = path+'\\'+i
         fp = open(i,"r")
-        #print(i+"文件打开成功")
+        print(i+"文件读取成功")
         strr = fp.read()
         fp.close()
         str=strr.splitlines()
@@ -21,8 +23,10 @@ for i in get_dir:
         for j in str:
             #print(j)
             if find in j:
-                print(j)
+                num+=1
                 print(j,file=wp)
     except IOError:
         print(i+"文件打开错误")
 wp.close()
+print("共读取到",num,"条日志")
+webbrowser.open(path+'\\'+'find.txt')
